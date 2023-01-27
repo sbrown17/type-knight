ee23824eecb8f9bede82b12fefe6f6047fcd9575
Io <stubrown357@gmail.com>
2023-01-26 20:36:47 -0600

    build out primitive bracket counter and depth finder

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ import { readFileSync } from 'node:fs';
 console.log("Initializing type-knight...");
 // access js file and get back string
 const file = readFileSync('testFiles/test.js', 'utf-8');
+const fileArray = file.split('');
+function findVariable(fileArray) {
+    
+}
+function findDepth(fileArray, fileIndex) {
+    const bracketsOpened = bracketCounter(fileArray, '{');
+    const bracketsClosed = bracketCounter(fileArray, '}');
+    return bracketsOpened - bracketsClosed;
+}
+
+function bracketCounter(fileArray, targetChar) {
+    let count = 0;
+    for(x in fileArray){
+        if (x === targetChar)
+            count++;
+    }
+    return count;
+}
 // count open "{" vs closed "}"
 // search for var, let, const
     // leave vars in for loops alone
