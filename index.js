3542209e999aabda9413f5ba1fe74cb5e52d9140
Io <stubrown357@gmail.com>
2023-02-07 20:29:45 -0600

    fix depth checker a bit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs';
 
 console.log("Initializing type-knight...");
 // access js file and get back string
-const file = readFileSync('testFiles/test.js', 'utf-8');
+const file = readFileSync('testFiles/test2.js', 'utf-8');
 console.log('test.js file: ', file);
 const fileArray = file.split('');
 console.log('file array: ', fileArray);
@@ -11,27 +11,28 @@ function findVariable(file, fileArray) {
     // find instance of var
     // get name of var
     // check if it is mutated
-    const foundVar = file.indexOf('var');
-    console.log('found var:', foundVar);
-    if (foundVar >= 0)
-        console.log(findDepth(fileArray, foundVar));
+    const foundVarIndex = file.indexOf('var');
+    console.log('found var: ', foundVarIndex);
+    if (foundVarIndex >= 0)
+        console.log('Depth: ', findDepth(foundVarIndex));
 }
 
-function findDepth(fileArray, fileIndex) {
-    const bracketsOpened = bracketCounter(fileArray, '{');
+function findDepth(foundVarIndex) {
+    const bracketsOpened = bracketCounter(foundVarIndex, '{');
     console.log('Opened Brackets: ', bracketsOpened);
-    const bracketsClosed = bracketCounter(fileArray, '}');
+    const bracketsClosed = bracketCounter(foundVarIndex, '}');
     console.log('Closed Brackets: ', bracketsClosed);
     if (!bracketsOpened)
         return 0;     
     return bracketsOpened - bracketsClosed;
 }
 
-function bracketCounter(fileArray, targetChar) {
+function bracketCounter(foundVarIndex, targetChar) {
     console.log('Counting ' + targetChar + ' brackets');
     let count = 0;
     for (const x in fileArray) {
-        if (fileArray[x] === targetChar)
+        if (x <= foundVarIndex &&
+            fileArray[x] === targetChar)
             count++;
     }
     console.log('Found ' + count + ' ' + targetChar + ' brackets');
---
+++ testFiles/test2.js
@@ -0,0 +1,7 @@
+let exampleVariable;
+
+for (let i; i < 10; i++) {
+    var a = 0;
+    exampleVariable += 1;
+    console.log(exampeleVariable)l
+}
